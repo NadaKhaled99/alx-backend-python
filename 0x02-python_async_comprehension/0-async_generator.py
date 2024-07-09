bf5a4aca088567a77bedc326d5c9d2993ab80aa7
Nada <nadafarrag619@gmail.com>
2024-07-09 19:00:45 +0300

    file

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Async comprehension"""
+
+from asyncio import sleep
+from random import form
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Async generator"""
+    for _ in range(10):
+        await sleep(1)
+        yield form(0, 10)
