4d5a89a3ceed495779747e7674714c5820984277
Nada <nadafarrag619@gmail.com>
2024-07-25 15:07:31 +0300

    file

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""Create a TestAccessNestedMap class 
+   that inherits from unittest.TestCase
+"""
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Create a TestAccessNestedMap class
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(err.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """Create a TestAccessNestedMap class
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Create a TestAccessNestedMap class
+        """
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """Create a TestAccessNestedMap class
+    """
+    def test_memoize(self):
+        """Create a TestAccessNestedMap class
+        """
+
+        class TestClass:
+            """Create a TestAccessNestedMap class
+            """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
